128b638f0f9b401d154501fddabc817f7f1b8214
iammahir2020 <1810659@iub.edu.bd>
2022-05-19 00:27:43 +0600

    add and get task routes added

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lim1g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const taskCollection = client.db("toToApp").collection("tasks");
+
+    app.get("/task", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        email: email,
+      };
+      const tasks = await taskCollection.find(query).toArray();
+      res.send(tasks);
+    });
+
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      console.log(task);
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("To Do App server is runing");
